ae6ea4a7a6f3b3a77eae3da7385104f8ad16ff08
topheruk <kristopherab@gmail.com>
2022-06-17 18:07:32 +0000

    rm

--- build/query.sql
+++
@@ -1,9 +0,0 @@
--- name: ListBooks :many
-SELECT *
-FROM book;
--- name: ListReviews :many
-SELECT *
-FROM review;
--- name: ListAuthors :many
-SELECT *
-FROM author;
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go.topheruk.bookcatalogue
 
 go 1.18
+
+require gotest.tools/v3 v3.2.0
+
+require github.com/google/go-cmp v0.5.5 // indirect
